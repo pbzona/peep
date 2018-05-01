4d56f1f97b77634f2ccd3e13c9b48b193bcedc55
Phil Zona <phil.b.zona@gmail.com>
2018-05-01 12:08:18 -0500

    Add comments for the main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,5 @@
+// This file is for pulling in JS files from the `scripts` folder
+// Include any scripts here that will be used on every part of the blog, not page specfic scripts
+// Run the js:build script to bundle this into `bundle.js` for inclusion on the pages
 (require('./scripts/insertDate'))();
 (require('./scripts/activePage'))();
